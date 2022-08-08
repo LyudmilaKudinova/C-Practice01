a86647e569db5e7347fe77fdf757b2135827e838
lyudmila <kudinova@alio-soft.ru>
2022-08-08 18:38:56 +0400

    Задача ДЕЛЕНИЕ на 2 без остатка

---
+++ C#Pr01Task03Dif/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Введите любое целое число  ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a % 2 == 0) 
+{
+    Console.WriteLine("Ура! Это чётное число.");
+}
+
+else
+{
+    Console.WriteLine("Упсс... Это не чётное число.");
+}
