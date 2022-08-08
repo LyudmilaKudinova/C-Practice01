e0c4f0f7e04ff6750210d0394b5664f9673ade49
lyudmila <kudinova@alio-soft.ru>
2022-08-08 12:39:57 +0400

    add C#Pr01Task01 Максимальное из двух чисел

---
+++ C#Pr01Task01_max/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача №2.Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if(b > max)
+{
+    Console.Write("Наибольшее число: ");
+    Console.WriteLine(b);
+    Console.Write("Наименьшее число: ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("Наибольшее число: ");
+    Console.WriteLine(a);
+    Console.Write("Наименьшее число: ");
+    Console.WriteLine(b); 
+}   
+
+// Проверка выполнена a = 5;  b = 7 -> max = 7  
+                   // a = 2;  b = 10 -> max = 10
+                    //a = -9; b = -3 -> max = -3
